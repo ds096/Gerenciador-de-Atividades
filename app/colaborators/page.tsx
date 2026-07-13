0bfc2647ae62c5fb5f61fb251a5040f52eb85721
ds096 <ss.daniel096@gmail.com>
2026-07-13 16:15:59 -0300

    fix bug when delete colaborator

--- app/colaborators/page.tsx
+++ app/colaborators/page.tsx
@@ -2,22 +2,16 @@
 import MenuItens from "@/components/MenuItens";
 import { SetColaboratorButton } from "@/components/ColaboratorSetButton";
 import ColaboratorsTable from "@/components/ColaboratorTable";
-import { useRef } from "react";
-import { ColaboratorData } from "@/public/colaboratorInterface";
 
 export default function Colaborators() {
-  const tableRef = useRef(null);
-  const handleCreateColaborator = (colaborator: ColaboratorData) => {
-    tableRef.current?.addColaborator(colaborator);
-  };
   return (
     <div>
       <div>
         <MenuItens selectedKey="colaborators" />
       </div>
       <div className="p-6 space-y-4">
-        <SetColaboratorButton onSuccess={handleCreateColaborator} />
-        <ColaboratorsTable ref={tableRef} />
+        <SetColaboratorButton />
+        <ColaboratorsTable />
       </div>
     </div>
   );
