3c7a5d097922592bff952d7c20e74237d04495be
ds096 <ss.daniel096@gmail.com>
2026-07-17 15:02:53 -0300

    Mudança de estratégia

--- app/activities/page.tsx
+++ app/activities/page.tsx
@@ -8,36 +8,15 @@ import { deleteActivitie, getActivities } from "@/api/activities";
 import { notification } from "antd";
 
 export default function Activities() {
-  const [activities, setActivities] = useState<ActivityData[]>([]);
+  const [data, setData] = useState<ActivityData[]>([]);
 
   useEffect(() => {
     async function fetchActivities() {
       const activities = await getActivities();
-      setActivities(activities);
+      setData(activities);
     }
     fetchActivities();
   }, []);
-
-  async function handleDeleteClick(recordId: string) {
-    try {
-      await deleteActivitie(recordId);
-      setActivities((prev) => prev.filter((a) => a.id !== recordId));
-      notification.success({
-        message: "Atividade deletada com sucesso",
-        placement: "topRight",
-        duration: 1,
-      });
-    } catch (error) {
-      notification.error({
-        message: "Error ao deletar atividade",
-        placement: "topRight",
-        duration: 1,
-      });
-      throw error;
-    }
-  }
-
-  async function handleSave() {}
 
   return (
     <div>
@@ -46,10 +25,7 @@ export default function Activities() {
       </div>
       <div className="p-6 space-y-4">
         <ActivitySetButton />
-        <ActivityTable
-          activities={activities}
-          handleDeleteClick={handleDeleteClick}
-        />
+        <ActivityTable data={data} />
       </div>
     </div>
   );
